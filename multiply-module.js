4100395994ef49ff6befba46f5c8d5c31ba7733f
zsobe31 <35228335+zsobe31@users.noreply.github.com>
2018-03-07 17:43:52 +0100

    Add files via upload

---
+++ multiply-module.js
@@ -0,0 +1,10 @@
+
+function logProduct(a,b) {
+	console.log(a*b);
+}
+
+var moduleMessage = 'The product is show on the console!';
+
+module.exports.logProduct = logProduct;
+module.exports.moduleMessage = moduleMessage;
+
