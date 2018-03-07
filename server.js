81b26f4ae502c4ef0965bfa61758c3a8bba68806
zsobe31 <35228335+zsobe31@users.noreply.github.com>
2018-03-07 15:28:31 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+function onRequest(request, response){
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Hello world');
+  response.end();
+}
+
+http.createServer(onRequest).listen(3000);
+console.log('The server is running...');
