6188f19de9c704f1aee3336943f239614713e1b8
zsobe31 <35228335+zsobe31@users.noreply.github.com>
2018-03-08 08:43:13 +0100

    Add files via upload

--- multiply-module.js
+++ multiply-module.js
@@ -1,10 +1,16 @@
 
-function logProduct(a,b) {
-	console.log(a*b);
+// function logProduct(a,b) {
+// 	console.log(a*b);
+// }
+
+// var moduleMessage = 'The product is show on the console!';
+
+// module.exports.logProduct = logProduct;
+// module.exports.moduleMessage = moduleMessage;
+
+module.exports = {
+  logProduct: function(a) {
+    console.log(a*a*a);
+  },
+  moduleMessage: 'The product is shown on the console. :)'
 }
-
-var moduleMessage = 'The product is show on the console!';
-
-module.exports.logProduct = logProduct;
-module.exports.moduleMessage = moduleMessage;
-
--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var http = require('http');
 var multiply = require('./multiply-module');
+var fs = require('fs');
+
 
 function onRequest(request, response){
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write(multiply.moduleMessage);
-  multiply.logProduct(3, 14);
-  response.end();
+//  response.write(multiply.moduleMessage);
+//  multiply.logProduct(7);
+//  response.end();
+//}
+//
+//http.createServer(onRequest).listen(3000);
+//console.log('The server is running...');
+fs.readFile('./index.html', null, function(error, data) {
+	if (error) {
+		response.writeHead(404);
+		response.write('File not found');
+	} else {
+		response.write(data);
+	}
+	response.end();
+});
 }
-
-http.createServer(onRequest).listen(3000);
-console.log('The server is running...');
